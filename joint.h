14e52636979d04dc7fe273391569d15b65f07b8c
Martino Ferrari <martino@manda-desktop.(none)>
2012-01-12 18:39:51 +0100

    Comment to joint class

--- joint.h
+++ joint.h
@@ -11,16 +11,16 @@ class Joint : public QObject
 {
     Q_OBJECT
 public:
-    explicit Joint(Goo* a,Goo* b, b2World * world,QObject *parent = 0);
-    void paint(QPainter &p);
-    b2Joint* getJoint();
-    bool has(Goo*a,Goo*b);
+    explicit Joint(Goo* a,Goo* b, b2World * world,QObject *parent = 0); //cunstractor: the two goo to link and the phisical world
+    void paint(QPainter &p); //Paint function
+    b2Joint* getJoint(); //To have acces at the material b2joint
+    bool has(Goo*a,Goo*b); //To check if this joint link this two GOO
 protected:
-    b2DistanceJoint* joint;
-    Goo *a,*b;
-    virtual void status();
+    b2DistanceJoint* joint; //The phisical joint
+    Goo *a,*b; //the two linked goo
+    virtual void status(); // function to check the status of the joint
 signals:
-    void destroyJoint(Joint *j);
+    void destroyJoint(Joint *j); //SIGNAL FOR DESTROY THIS JOINT
 public slots:
 
 };
