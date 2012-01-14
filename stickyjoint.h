bb9496d92288430a47c94a11a05304d5705b00fa
Martino Ferrari <manda.mgf@gmail.com>
2012-01-14 19:16:28 +0100

    Start to work around stiky joint. To make the goo sticky at the ground!

--- stickyjoint.h
+++ stickyjoint.h
@@ -1,25 +1,36 @@
-//#ifndef STICKYJOINT_H
-//#define STICKYJOINT_H
+#ifndef STICKYJOINT_H
+#define STICKYJOINT_H
 
 
-//#include "joint.h"
+#include <QObject>
 
 
-////This joint is made to simulate the balls sticked to the ground
-////When a fixed goo touch the ground a Sticky joint will be created between the goo and the contact point!
+#include "goo.h"
+#include "ground.h"
 
-////The final joint will be invisible
+#include <Box2D/Box2D.h>
 
-//class StickyJoint : public Joint
-//{
-//    Q_OBJECT
-//public:
-//    explicit StickyJoint(QObject *parent = 0);
+//This joint is made to simulate the balls sticked to the ground
+//When a fixed goo touch the ground a Sticky joint will be created between the goo and the contact point!
 
-//signals:
 
-//public slots:
+class StickyJoint : public QObject
+{
+    Q_OBJECT
+public:
+    explicit StickyJoint(Goo* goo, Ground* ground,QPoint point,QObject *parent = 0);
+    void checkStatus();
+    //TEMP
+    void paint(QPainter &p);
+private:
+    b2DistanceJoint *joint;
+    Goo* goo;
+    Ground* ground;
+    QPoint point;
+signals:
+    void destroyStiky();
+public slots:
 
-//};
+};
 
-//#endif // STICKYJOINT_H
+#endif // STICKYJOINT_H
--- tools.h
+++ tools.h
@@ -10,21 +10,21 @@
 #include <Box2D/Box2D.h>
 
 //Conversion for qt/box2d
-b2Vec2 toVec(QPoint p);
-QPoint toPoint(b2Vec2 v);
+b2Vec2 toVec(QPoint p); //Convert a QPoint in a b2Vec2
+QPoint toPoint(b2Vec2 v); //Convert a b2Vec2 in a QPoint
 
-b2Vec2* toVecs(QList<QPoint> ps);
-QPolygon toPoly(QList<QPoint> ps);
-QPolygon toPoly(QList<QPoint> ps,QPoint center);
+b2Vec2* toVecs(QList<QPoint> ps); //Convert a QList of QPoint in an array of b2Vec2
+QPolygon toPoly(QList<QPoint> ps); //Convert a QList of QPoint in a QPolygon
+QPolygon toPoly(QList<QPoint> ps,QPoint center); //Convert a QList of QPoint in a QPolygon transated of *center*
 
 
 
 //Level file parse function
 
-QPoint pPoint(QString s, bool &ok,int w=1000,int h=1000);
-QSize pSize(QString s, bool &ok);
-QRect pRect(QString s, bool &ok, int w=1000,int h=1000);
-QList <QPoint> pList(QString s, bool&ok,int w=1000,int h=1000, int min=3);
+QPoint pPoint(QString s, bool &ok,int w=1000,int h=1000); //Parse a string "X Y" in a QPoint(x,y) with cordinate scaled of 1000/w and 1000/h
+QSize pSize(QString s, bool &ok); //parse a string "W H" in a QSize(w,h)
+QRect pRect(QString s, bool &ok, int w=1000,int h=1000); //Parse a string "X Y,W H" in a QRect(x,y,w,h) with cordinade scaled of 1000/w and 1000/h
+QList <QPoint> pList(QString s, bool&ok,int w=1000,int h=1000, int min=3); //Parse a string "X0 Y0,X1 Y1,...,XN YN" in a QList of QPoint scaled of 1000/w and 1000/h
 
 
 #endif // TOOLS_H
