2fc79631d21002e8278ff9ffe8b360107dbf0888
Martino Ferrari <manda.mgf@gmail.com>
2012-02-23 17:48:09 +0100

    A fix at one of the target check and some comments.

--- ground.cpp
+++ ground.cpp
@@ -2,32 +2,45 @@
 #include "tools.h"
 #include <QDebug>
 
+
+//Constructor
 Ground::Ground(b2World *world, QPoint center, QList<QPoint> shape, QObject *parent):
     QObject(parent)
 {
     //Body definition
     b2BodyDef def;
-    def.type=b2_staticBody; //b2_staticBody ignore all forces and interact only with dynamic body.
-    def.position=toVec(center); //center of the body
-    body= world->CreateBody(&def); //create the body;
-    makeShape(shape); //create the shape
+    //b2_staticBody ignore all forces and interact only with dynamic body.
+    def.type=b2_staticBody;
+    //center of the body
+    def.position=toVec(center);
+    //create the body;
+    body= world->CreateBody(&def);
+    //create the shape
+    makeShape(shape);
 
-    polShape=toPoly(shape,center); //save the shape
+    //save the shape to a QPlygon for draw it.
+    polShape=toPoly(shape,center);
 }
 
+//Return physical body of the ground
 b2Body* Ground::getBody(){
-    return body; //Return physical body
+    return body;
 }
 
+//Function to paint the ground
 void Ground::paint(QPainter &p){
     p.setBrush(Qt::black);
     p.setPen(Qt::black);
-    p.drawPolygon(polShape); //Dray the shape
+    //Draw the shape
+    p.drawPolygon(polShape);
 }
 
+//Function to create the shape starting from the points read from the level files
 void Ground::makeShape(QList<QPoint> points){
-    b2EdgeShape* shape; //b2EdgeShape is a shape made of segments
-    for (int i=0;i<points.length()-1;i++){ //make a segment for all the points
+    //b2EdgeShape is a shape made of segments
+    b2EdgeShape* shape;
+    //make a segment for all the points
+    for (int i=0;i<points.length()-1;i++){
         shape=new b2EdgeShape();
         shape->Set(toVec(points[i]),toVec(points[i+1]));
         body->CreateFixture(shape,1.0);
