b7215a0e18502e720cf344c3d29be5d8e2fe048b
Martino Ferrari <manda.mgf@gmail.com>
2012-06-21 14:38:39 +0200

    [CORE]Fix at flag fps

--- flags.h
+++ flags.h
@@ -6,7 +6,7 @@ static const unsigned char STANDARD  = 0;
 static const unsigned char DEBUG     = 1<<0;
 static const unsigned char OPENGL    = 1<<1;
 static const unsigned char ONLYTEXT  = 1<<2;
-static const unsigned char FPS       = 2<<3;
+static const unsigned char FPS       = 1<<3;
 
 extern unsigned char flag;
 
