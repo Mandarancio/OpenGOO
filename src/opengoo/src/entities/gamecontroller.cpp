167d5ea50f85274252475e87e5343d42a7ce1737
Dmitry Batarov <nonetype@gmail.com>
2016-07-13 23:54:36 +0300

    updated gamecontroller

--- src/opengoo/src/entities/gamecontroller.cpp
+++ src/opengoo/src/entities/gamecontroller.cpp
@@ -7,9 +7,12 @@
 
 GameController::GameController(GameData& a_gdata)
     : Entity(0, 0),
-      m_pipe(nullptr),
       m_gameData(a_gdata)
 { 
+    m_ballsRequired = 0;
+    m_balls = 0;
+    m_hasLevelExit = false;
+    m_isShowProgress = false;
 }
 
 GameController::~GameController()
@@ -26,6 +29,7 @@ void GameController::Added()
         auto e = m_gameData.efactory->CreateExit(*m_gameData.levelexit, this);
         GetScene()->AddEntity(e);
         m_ballsRequired = m_gameData.ballsrequired;
+        m_hasLevelExit = true;
     }
 }
 
@@ -44,4 +48,39 @@ void GameController::OnClosed()
     if (m_pipe)
         m_pipe->Close();
 }
+
+void GameController::OnBallExit()
+{
+    ++m_balls;
+
+    if (m_ballsRequired > 0 && m_balls == m_ballsRequired)
+        m_isShowProgress = true;
+}
+
+void GameController::Render(QPainter& a_painter)
+{
+    if (m_hasLevelExit)
+    {
+        a_painter.save();
+        a_painter.resetTransform();
+        QFont font("Times", 14, QFont::Bold);
+        a_painter.setPen(Qt::white);
+        a_painter.setFont(font);
+        a_painter.drawText(10, 20, QString::number(m_balls));
+
+        if (m_isShowProgress)
+        {
+            QString text("Congratulations");
+            QFont font("Times", 24, QFont::Bold);
+            a_painter.setFont(font);
+            auto&& fm = a_painter.fontMetrics();
+            auto xx = 400 - fm.width(text) / 2;
+            auto yy = 300 - fm.height() / 2;
+            a_painter.drawText(xx, yy, text);
+        }
+
+        a_painter.restore();
+    }
+}
+
 
--- src/opengoo/src/entities/gamecontroller.h
+++ src/opengoo/src/entities/gamecontroller.h
@@ -11,13 +11,19 @@ class GameController : public og::Entity, public ExitEventListener
     std::unique_ptr<OGIPipe> m_pipe;
     int m_ballsRequired;
     GameData m_gameData;
+    bool m_hasLevelExit;
+    int m_balls;
+    bool m_isShowProgress;
 
     void OnOpen();
     void OnClosed();
+    void OnBallExit();
 
     void Update();
 
     void Added();
+
+    void Render(QPainter& a_painter);
 
 public:
     GameController(GameData& a_gdata);
